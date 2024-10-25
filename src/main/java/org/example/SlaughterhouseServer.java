bafdea0317bb6c2a84da81ec6cc353d7d34bd62e
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2024-10-25 10:46:46 +0200

    changes again 25/10

--- src/main/java/org/example/SlaughterhouseServer.java
+++ src/main/java/org/example/SlaughterhouseServer.java
@@ -4,31 +4,41 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 import java.sql.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import io.grpc.stub.StreamObserver;
 import com.example.grpc.SlaughterhouseProto.Animal;
 import com.example.grpc.SlaughterhouseProto.Product;
 import com.example.grpc.SlaughterhouseServiceGrpc;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 import static java.lang.Thread.sleep;
 
-public class SlaughterhouseServer
-{
+public class SlaughterhouseServer {
 
-  // Simulated database
-  private static Connection connect() throws SQLException {
-    String url = "jdbc:postgresql://localhost:5432/postgres";
-    String user = "postgres";
-    String password = "451311qq";
+  private static Connection connect() throws SQLException, IOException {
+    Properties props = new Properties();
+    try (InputStream input = SlaughterhouseServer.class.getClassLoader().getResourceAsStream("config.properties")) {
+      if (input == null) {
+        throw new IOException("Unable to find config.properties");
+      }
+      props.load(input);
+    }
+
+    String url = props.getProperty("db.url");
+    String user = props.getProperty("db.user");
+    String password = props.getProperty("db.password");
+
     return DriverManager.getConnection(url, user, password);
   }
+
   private static final Map<Integer, Animal> animals = new HashMap<>();
-  private static  final Map<Integer, Product> products = new HashMap<>();
+  private static final Map<Integer, Product> products = new HashMap<>();
 
   public static void main(String[] args)
           throws IOException, InterruptedException, SQLException {
@@ -36,8 +46,8 @@ public class SlaughterhouseServer
     initializeData();
 
     Server server = ServerBuilder.forPort(50051).addService(
-            new SlaughterhouseServiceImpl())  // implementation for the services
-        .build();
+                    new SlaughterhouseServiceImpl())  // implementation for the services
+            .build();
 
     // Start the server
     server.start();
@@ -46,21 +56,14 @@ public class SlaughterhouseServer
     System.out.println("Welcome to the Slaughterhouse! ");
     sleep(1000);
     System.out.println("To view current animal livestock, press C to continue:");
-     Scanner scanner = new Scanner(System.in);
-     if (scanner.nextLine().equalsIgnoreCase("C")) {
-
-
-
-    for (Animal animal : animals.values())
-      System.out.println("Loaded Animal: ID=" + animal.getId() + ", Species=" + animal.getSpecies() + ", Weight=" + animal.getWeight());
+    Scanner scanner = new Scanner(System.in);
+    if (scanner.nextLine().equalsIgnoreCase("C")) {
+      for (Animal animal : animals.values())
+        System.out.println("Loaded Animal: ID=" + animal.getId() + ", Species=" + animal.getSpecies() + ", Weight=" + animal.getWeight());
     }
-
-
-
 
     // Stop the server
     server.shutdownNow();
-    //Scanner scanner = new Scanner(System.in);
     if(scanner.nextLine().equalsIgnoreCase("exit"))
       System.out.println("Shutting down the server...");
 
@@ -68,10 +71,9 @@ public class SlaughterhouseServer
     server.awaitTermination();
   }
 
-  private static void initializeData() throws SQLException {
-
+  private static void initializeData() throws SQLException, IOException {
     try (Connection connection = connect()) {
-      String animalQuery = "SELECT  ID, Species,Weight  FROM slaughterhouse.animal";
+      String animalQuery = "SELECT ID, Species, Weight FROM slaughterhouse.animal";
       try {
         PreparedStatement statement = connection.prepareStatement(animalQuery);
         ResultSet resultSet = statement.executeQuery();
@@ -81,101 +83,62 @@ public class SlaughterhouseServer
           double weight = resultSet.getDouble("weight");
 
           animals.put(id, Animal.newBuilder().setId(id).setSpecies(species)
-             .setWeight(weight).build());
-
-
+                  .setWeight(weight).build());
         }
-
         resultSet.close();
-
-
-      }
-      catch (SQLException e) {
+      } catch (SQLException e) {
         e.printStackTrace();
       }
     }
-    // Adding sample shit
-    animals.put(1,
-        Animal.newBuilder().setId(1).setRegistrationNumber("ANIMAL001")
-            .setSpecies(":Cow").setWeight(500).build());
-    animals.put(2,
-        Animal.newBuilder().setId(2).setRegistrationNumber("ANIMAL002")
-            .setSpecies(":Pig").setWeight(250).build());
-    animals.put(3,
-            Animal.newBuilder().setId(3).setRegistrationNumber("ANIMAL003")
-            .setSpecies(":Sheep").setWeight(300).build());
-    animals.put(4,
-            Animal.newBuilder().setId(4).setRegistrationNumber("ANIMAL004")
-           .setSpecies(":Goat").setWeight(400).build());
-    animals.put(5,
-            Animal.newBuilder().setId(5).setRegistrationNumber("ANIMAL005")
-           .setSpecies(":Chicken").setWeight(20).build());
-    animals.put(6,
-            Animal.newBuilder().setId(6).setRegistrationNumber("ANIMAL006")
-           .setSpecies(":Deer").setWeight(70).build());
-    animals.put(7,
-            Animal.newBuilder().setId(7).setRegistrationNumber("ANIMAL007")
-           .setSpecies(":Rabbit").setWeight(10).build());
-    animals.put(8,
-            Animal.newBuilder().setId(8).setRegistrationNumber("ANIMAL008")
-           .setSpecies(":Duck").setWeight(15).build());
 
-
+    // Adding sample data
+    animals.put(1, Animal.newBuilder().setId(1).setRegistrationNumber("ANIMAL001").setSpecies(":Cow").setWeight(500).build());
+    animals.put(2, Animal.newBuilder().setId(2).setRegistrationNumber("ANIMAL002").setSpecies(":Pig").setWeight(250).build());
+    animals.put(3, Animal.newBuilder().setId(3).setRegistrationNumber("ANIMAL003").setSpecies(":Sheep").setWeight(300).build());
+    animals.put(4, Animal.newBuilder().setId(4).setRegistrationNumber("ANIMAL004").setSpecies(":Goat").setWeight(400).build());
+    animals.put(5, Animal.newBuilder().setId(5).setRegistrationNumber("ANIMAL005").setSpecies(":Chicken").setWeight(20).build());
+    animals.put(6, Animal.newBuilder().setId(6).setRegistrationNumber("ANIMAL006").setSpecies(":Deer").setWeight(70).build());
+    animals.put(7, Animal.newBuilder().setId(7).setRegistrationNumber("ANIMAL007").setSpecies(":Rabbit").setWeight(10).build());
+    animals.put(8, Animal.newBuilder().setId(8).setRegistrationNumber("ANIMAL008").setSpecies(":Duck").setWeight(15).build());
 
     // Adding sample products (associated with animals)
     products.put(1, Product.newBuilder().setId(1).addAnimalIds(1).build());
     products.put(2, Product.newBuilder().setId(2).addAnimalIds(2).build());
   }
 
-  static class SlaughterhouseServiceImpl
-      extends SlaughterhouseServiceGrpc.SlaughterhouseServiceImplBase
-  {
+  static class SlaughterhouseServiceImpl extends SlaughterhouseServiceGrpc.SlaughterhouseServiceImplBase {
 
-    @Override public void getAnimalInfo(Product request,
-        StreamObserver<Animal> responseObserver) // client gets the information about animals
-    {
-      if (request.getAnimalIdsCount() > 0)
-      {
+    @Override
+    public void getAnimalInfo(Product request, StreamObserver<Animal> responseObserver) {
+      if (request.getAnimalIdsCount() > 0) {
         int animalId = request.getAnimalIds(0);  // Get the first animal ID
-        Animal animal = animals.get(
-            animalId);   // from the "database"
+        Animal animal = animals.get(animalId);   // from the "database"
 
-        if (animal != null)
-        {
+        if (animal != null) {
           // Send the animal information
           responseObserver.onNext(animal);
-        }
-        else
-        {
+        } else {
           responseObserver.onError(new Exception("Animal not found"));
         }
-      }
-      else
-      {
-        responseObserver.onError(
-            new Exception("No animal IDs associated with the product"));
+      } else {
+        responseObserver.onError(new Exception("No animal IDs associated with the product"));
       }
       responseObserver.onCompleted();
     }
 
-    @Override public void getProductInfo(Animal request,
-        StreamObserver<Product> responseObserver)
-    {
-
+    @Override
+    public void getProductInfo(Animal request, StreamObserver<Product> responseObserver) {
       int animalId = request.getId();
 
-      for (Product product : products.values())
-      {
-        if (product.getAnimalIdsList().contains(animalId))
-        {
+      for (Product product : products.values()) {
+        if (product.getAnimalIdsList().contains(animalId)) {
           // Send the product information
           responseObserver.onNext(product);
           responseObserver.onCompleted();
           return;
         }
       }
-      responseObserver.onError(
-          new Exception("No product found for this animal"));
+      responseObserver.onError(new Exception("No product found for this animal"));
     }
   }
 }
