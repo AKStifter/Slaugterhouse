f5191cc16e2c1271773eba6969da82f42faa77fd
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2024-11-06 22:24:19 +0100

    changes

--- src/main/java/org/example/SlaughterhouseServer.java
+++ src/main/java/org/example/SlaughterhouseServer.java
@@ -42,29 +42,136 @@ public class SlaughterhouseServer implements CommandLineRunner {
     public static final Map<Integer, SlaughterhouseProto.Tray> trays = new HashMap<>();
 
     private void initializeData() throws SQLException {
-        String animalQuery = "SELECT ID, Species, Weight FROM slaughterhouse.animal";
+
+        String animalQuery = "SELECT ID, Species, Weight, arrivaldate,recall_id FROM slaughterhouse.animal";
         try {
             jdbcTemplate.query(animalQuery, (rs, rowNum) -> {
-                int id = rs.getInt("id");
-                String species = rs.getString("species");
-                double weight = rs.getDouble("weight");
+                int id = rs.getInt("ID");
+                String species = rs.getString("Species");
+                double weight = rs.getDouble("Weight");
+                String registrationNumber = rs.getString("Registration_Number");
 
-                animals.put(id, Animal.newBuilder().setId(id).setSpecies(species)
-                        .setWeight(weight).build());
+                animals.put(id, Animal.newBuilder()
+                        .setId(id)
+                        .setSpecies(species)
+                        .setWeight(weight)
+                        .setRegistrationNumber(registrationNumber)
+                        .build());
                 return null;
             });
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        // Adding sample data probably not needed now
-        animals.put(1, Animal.newBuilder().setId(1).setRegistrationNumber("ANIMAL001").setSpecies(":Cow").setWeight(500).build());
-        animals.put(2, Animal.newBuilder().setId(2).setRegistrationNumber("ANIMAL002").setSpecies(":Pig").setWeight(250).build());
-        // ... (other sample animals)
 
-        // Adding sample products (associated with animals) .. same as this
-        products.put(1, Product.newBuilder().setId(1).addAnimalIds(1).build());
-        products.put(2, Product.newBuilder().setId(2).addAnimalIds(2).build());
+        String productQuery = "SELECT ID FROM slaughterhouse.product";
+        try {
+            jdbcTemplate.query(productQuery, (rs, rowNum) -> {
+                int id = rs.getInt("ID");
+                products.put(id, Product.newBuilder().setId(id).build());
+                return null;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String partQuery = "SELECT ID, Animal_ID, weight,type FROM slaughterhouse.part";
+        try {
+            jdbcTemplate.query(partQuery, (rs, rowNum) -> {
+                int id = rs.getInt("ID");
+                int animalId = rs.getInt("Animal_ID");
+                String name = rs.getString("Name");
+
+                parts.put(id, Part.newBuilder()
+                        .setId(id)
+                        .setAnimalId(animalId)
+                        .setName(name)
+                        .build());
+                return null;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        String productAnimalQuery = "SELECT ID, type,tray_ids FROM slaughterhouse.productanimal";
+        try {
+            jdbcTemplate.query(productAnimalQuery, (rs, rowNum) -> {
+                int productId = rs.getInt("Product_ID");
+                int animalId = rs.getInt("Animal_ID");
+
+                ProductAnimal productAnimal = ProductAnimal.newBuilder()
+                        .setProductId(productId)
+                        .setAnimalId(animalId)
+                        .build();
+                productAnimals.put(productId * 10000 + animalId, productAnimal);
+                return null;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        String productPartQuery = "SELECT ID, type, max_weight,tray_ids,productiondate,status FROM slaughterhouse.productpart";
+        try {
+            jdbcTemplate.query(productPartQuery, (rs, rowNum) -> {
+                int productId = rs.getInt("Product_ID");
+                int partId = rs.getInt("Part_ID");
+
+                SlaughterhouseProto.ProductPart productPart = SlaughterhouseProto.ProductPart.newBuilder()
+                        .setProductId(productId)
+                        .setPartId(partId)
+                        .build();
+                productParts.put(productId * 10000 + partId, productPart);
+                return null;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String recallQuery = "SELECT ID, dateinitiated, Reason FROM slaughterhouse.recall";
+        try {
+            jdbcTemplate.query(recallQuery, (rs, rowNum) -> {
+                int id = rs.getInt("ID");
+                int productId = rs.getInt("Product_ID");
+                String reason = rs.getString("Reason");
+
+                recalls.put(id, Recall.newBuilder()
+                        .setId(id)
+                        .setProductId(productId)
+                        .setReason(reason)
+                        .build());
+                return null;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String trayQuery = "SELECT ID, type_of_part, Max_Weight,current_weight FROM slaughterhouse.tray";
+        try {
+            jdbcTemplate.query(trayQuery, (rs, rowNum) -> {
+                int id = rs.getInt("ID");
+                int productId = rs.getInt("Product_ID");
+                double maxWeight = rs.getDouble("Max_Weight");
+
+                trays.put(id, Tray.newBuilder()
+                        .setId(id)
+                        .setProductId(productId)
+                        .setMaxWeight(maxWeight)
+                        .build());
+                return null;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Adding sample data , this was just to see if i did things correctly
+        //animals.put(1, Animal.newBuilder().setId(1).setRegistrationNumber("ANIMAL001").setSpecies(":Cow").setWeight(500).build());
+        //animals.put(2, Animal.newBuilder().setId(2).setRegistrationNumber("ANIMAL002").setSpecies(":Pig").setWeight(250).build());
+
+        // Adding sample products same as above
+        //products.put(1, Product.newBuilder().setId(1).addAnimalIds(1).build());
+       // products.put(2, Product.newBuilder().setId(2).addAnimalIds(2).build());
     }
 
     @Override
@@ -73,43 +180,71 @@ public class SlaughterhouseServer implements CommandLineRunner {
 
         Server server = ServerBuilder.forPort(50051)
                 .addService(slaughterhouseService)
-                .build();     // It sets up our data and gRPC server.
+                .build();
 
-        // Start the server
         server.start();
-        System.out.println("Server started please wait:");
+        System.out.println("Server started, please wait:");
         sleep(3000);
-        System.out.println("Welcome to the Slaughterhouse! ");
+        System.out.println("Welcome to the Slaughterhouse!");
         sleep(1000);
-        System.out.println("To view current animal livestock, press C to continue:");
+
         Scanner scanner = new Scanner(System.in);
-        if (scanner.nextLine().equalsIgnoreCase("C")) {
-            for (Animal animal : animals.values())
-                System.out.println("Loaded Animal: ID=" + animal.getId() + ", Species=" + animal.getSpecies() + ", Weight=" + animal.getWeight());
-            System.out.println("View Available Products");
-            if (scanner.nextLine().equalsIgnoreCase("C" )){
-                for (Product product : products.values())
-                    System.out.println("Loaded Product: ID=" + product.getId() + ", Animals=" + product.getAnimalIdsList());
-            }
-            System.out.println("To view product parts");
-            if (scanner.nextLine().equalsIgnoreCase("C" )){
-                for (SlaughterhouseProto.ProductPart productPart : productParts.values())
-                    System.out.println("Loaded ProductPart: ID=" +  ", ProductID=" + productPart.getProductId() + ", PartID=" + productPart.getPartId());
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("\nChoose an option to view data (or type 'exit' to quit):");
+            System.out.println("1. View Animals");
+            System.out.println("2. View Products");
+            System.out.println("3. View Parts");
+            System.out.println("4. View Products by Animal ");
+            System.out.println("5. View Product parts /cuts ");
+            System.out.println("6. View Product Recalls");
+            System.out.println("7. View Trays after slaughter");
+
+            String input = scanner.nextLine();
+
+            switch (input.toLowerCase()) {
+                case "1":
+                    for (Animal animal : animals.values())
+                        System.out.println("Animal: ID=" + animal.getId() + ", Species=" + animal.getSpecies() + ", Weight=" + animal.getWeight());
+                    break;
+                case "2":
+                    for (Product product : products.values())
+                        System.out.println("Product: ID=" + product.getId() + ", Animals=" + product.getAnimalIdsList());
+                    break;
+                case "3":
+                    for (Part part : parts.values())
+                        System.out.println("Part: ID=" + part.getId() + ", AnimalID=" + part.getAnimalId() + ", Name=" + part.getName());
+                    break;
+                case "4":
+                    for (ProductAnimal productAnimal : productAnimals.values())
+                        System.out.println("ProductAnimal: ProductID=" + productAnimal.getProductId() + ", AnimalID=" + productAnimal.getAnimalId());
+                    break;
+                case "5":
+                    for (SlaughterhouseProto.ProductPart productPart : productParts.values())
+                        System.out.println("ProductPart: ProductID=" + productPart.getProductId() + ", PartID=" + productPart.getPartId());
+                    break;
+                case "6":
+                    for (Recall recall : recalls.values())
+                        System.out.println("Recall: ID=" + recall.getId() + ", ProductID=" + recall.getProductId() + ", Reason=" + recall.getReason());
+                    break;
+                case "7":
+                    for (Tray tray : trays.values())
+                        System.out.println("Tray: ID=" + tray.getId() + ", ProductID=" + tray.getProductId() + ", MaxWeight=" + tray.getMaxWeight());
+                    break;
+                case "exit":
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
             }
         }
 
-
-        // type 'exit' to stop the server in the terminal
-        while (!scanner.nextLine().equalsIgnoreCase("exit")) {
-            System.out.println("Type 'exit' to shut down the server.");
-        }
-
-        // Stops the server
         System.out.println("Shutting down the server...");
         if (server != null) {
             server.shutdown();
             try {
-                if (!server.awaitTermination(30,TimeUnit.SECONDS)) {
+                if (!server.awaitTermination(30, TimeUnit.SECONDS)) {
                     server.shutdownNow();
                     if (!server.awaitTermination(30, TimeUnit.SECONDS)) {
                         System.err.println("Server did not terminate");
@@ -120,9 +255,6 @@ public class SlaughterhouseServer implements CommandLineRunner {
                 Thread.currentThread().interrupt();
             }
         }
-        server.shutdownNow();
         System.exit(0);
-
-
     }
 }
